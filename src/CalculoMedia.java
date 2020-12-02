2fd3a7f6ea6d53198cc5ea7f69c63909eb18b36c
Alexandra Santos <aleseect@gmail.com>
2020-12-02 15:23:42 -0300

    Add class CalculoMedia

---
+++ src/CalculoMedia.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;//importa a classe Scanner
+
+public class CalculoMedia {
+    public static void main (String[] args){
+        //metodo scanner
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Digite o nome da(o) aluna(o)");
+        //
+
+
+    }
+}

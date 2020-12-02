49e469dd3bfcd2549d24bb5d53c8414da589d545
Alexandra Santos <aleseect@gmail.com>
2020-12-02 15:34:12 -0300

    Add input for name

--- src/CalculoMedia.java
+++ src/CalculoMedia.java
@@ -3,9 +3,11 @@ import java.util.Scanner;//importa a classe Scanner
 public class CalculoMedia {
     public static void main (String[] args){
         //metodo scanner
-        Scanner sn = new Scanner(System.in);
-        System.out.println("Digite o nome da(o) aluna(o)");
-        //
+        Scanner meuScanner = new Scanner(System.in);
+        System.out.println("Digite o nome da(o) aluna(o): ");
+        String nome = meuScanner.nextLine();//inserir os dados pelo teclado
+        //print na tela
+        System.out.println("O nome da aluna(o) é " +nome+" !");
 
 
     }

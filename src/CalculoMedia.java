7121010947f28fb15725f50d2d73f410a33a7647
Alexandra Santos <aleseect@gmail.com>
2020-12-02 15:53:20 -0300

    Add operators

--- src/CalculoMedia.java
+++ src/CalculoMedia.java
@@ -6,9 +6,21 @@ public class CalculoMedia {
         Scanner meuScanner = new Scanner(System.in);
         System.out.println("Digite o nome da(o) aluna(o): ");
         String nome = meuScanner.nextLine();//inserir os dados pelo teclado
-        //print na tela
-        System.out.println("O nome da aluna(o) é " +nome+" !");
-
+        //Inicio Saída de Dados
+        //System.out.println("************* SISTEMA GESTÃO ACADEMICA *************");
+        //System.out.println("O nome da aluna(o) é " +nome+" !");
+        //Inicio dados das provas
+        System.out.println("Digite a nota da primeira prova bimestral : ");
+        Float prova1 = meuScanner.nextFloat();
+        System.out.println("Digite a nota da segunda prova bimestral : ");
+        Float prova2 = meuScanner.nextFloat();
+        System.out.println("Digite a nota final dos exercícios complementares : ");
+        Float exercicios = meuScanner.nextFloat();
+        System.out.println("Digite a nota final dos projetos de Software : ");
+        Float projetos = meuScanner.nextFloat();
+        //Validar a Media
+        Float mediaFinal = (prova1 + prova2 + exercicios + projetos) /4;
+        System.out.println("A media da(o) " +nome+ " é de "+mediaFinal);
 
     }
 }
